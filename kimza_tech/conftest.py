616a4aa8dc3b7c1d395fd6d443ab097987ea04e4
abdikeem <abdihakimabdirahman21@mail.com>
2021-12-28 16:09:16 +0300

    initial commit

---
+++ kimza_tech/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from kimza_tech.users.models import User
+from kimza_tech.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
